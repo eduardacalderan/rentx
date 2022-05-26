9745d639871f187761c15eb4eab21ed571bcbf90
Eduarda Calderan <eduardanicolodicalderan@gmail.com>
2022-05-26 19:29:34 -0300

    using SRP (Single Responsability Principle)

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,12 +1,17 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
+
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+
+  createCategoryService.execute({ name, description });
 
   const categoryAlreadyExists = categoriesRepository.findByName(name);
 
