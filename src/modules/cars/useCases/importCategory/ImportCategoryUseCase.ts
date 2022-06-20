5bc052ac7bc805f8112da7e7f563445e590b6bf9
Eduarda Calderan <eduardanicolodicalderan@gmail.com>
2022-06-20 11:59:43 -0300

    finished documentation

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -11,7 +11,7 @@ class ImportCategoryUseCase {
   constructor(private categoriesRepository: ICategoriesRepository) {}
 
   loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
-    return new Promise((reslve, reject) => {
+    return new Promise((resolve, reject) => {
       const stream = fs.createReadStream(file.path);
       const categories: IImportCategory[] = [];
       const parseFile = csvParse();
@@ -24,7 +24,8 @@ class ImportCategoryUseCase {
           categories.push({ name, description });
         })
         .on("end", () => {
-          reslve(categories);
+          fs.promises.unlink(file.path);
+          resolve(categories);
         })
         .on("error", err => {
           reject(err);
