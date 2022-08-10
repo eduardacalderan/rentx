12c6634e63e2d3110cc7e449e7742a276a20498a
Eduarda Calderan <eduardanicolodicalderan@gmail.com>
2022-08-10 19:54:03 -0300

    creating cars specification migrations (many to many)

--- src/modules/accounts/infra/typeorm/repositories/UsersRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/UsersRepository.ts
@@ -1,6 +1,5 @@
 import { getRepository, Repository } from "typeorm";
 
-import { ICreateUserDTO } from "../../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../../repositories/IUsersRepository";
 import { User } from "../entities/User";
 
